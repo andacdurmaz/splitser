70c4001a2edd042c0f36141f7837be93e24a1ef9
Jort Kuipers <j.h.j.kuipers@student.tudelft.nl>
2024-02-21 21:31:10 +0100

    allowing the user to change and see the title of the event, as well as showing the number of participants and a future skeleton getter and setter for the expenses

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -1,5 +1,42 @@
 package commons;
 
 public class Event {
-    
+    private String title;
+    private int amountOfParticipants;
+
+    /*
+    Change to Expenses class whenever that has been made
+     */
+    private String expense;
+
+
+    public Event(String title, int amountOfParticipants, String expense) {
+        this.title = title;
+        this.amountOfParticipants = amountOfParticipants;
+        this.expense = expense;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getAmountOfParticipants() {
+        return amountOfParticipants;
+    }
+
+    public void setAmountOfParticipants(int amountOfParticipants) {
+        this.amountOfParticipants = amountOfParticipants;
+    }
+
+    public String getExpense() {
+        return expense;
+    }
+
+    public void setExpense(String expense) {
+        this.expense = expense;
+    }
 }
