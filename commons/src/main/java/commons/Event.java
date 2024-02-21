0300bb59b199333e725936de75321e020533ad53
Jort Kuipers <j.h.j.kuipers@student.tudelft.nl>
2024-02-21 22:13:39 +0100

    Skeleton code for the Event class, and making a title for the events

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -1,5 +1,47 @@
 package commons;
 
 public class Event {
-    
+    private String title;
+    private int amountOfParticipants;
+
+    /*
+    not in use yet, for when the Expense class has been made
+     */
+    //private List<Expense> Expenses;
+
+    /*
+    Constructor for the Event class
+    @param title, amountOfParticipants
+     */
+    public Event(String title, int amountOfParticipants) {
+        this.title = title;
+        this.amountOfParticipants = amountOfParticipants;
+    }
+
+    /*
+    gets the title of the event
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /*
+    lets the user set the title for the event
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    /*
+    shows the amount of participants
+     */
+    public int getAmountOfParticipants() {
+        return amountOfParticipants;
+    }
+
+    /*
+    lets the user input the amount of participants
+     */
+    public void setAmountOfParticipants(int amountOfParticipants) {
+        this.amountOfParticipants = amountOfParticipants;
+    }
 }
