34eb5aae21b0ee509f8a34524d820dbc9478f2e1
tcoman <T.A.Coman@student.tudelft.nl>
2024-02-22 22:23:49 +0100

    Serer class testing + additional methods creation

--- server/src/test/java/server/ServTest.java
+++ server/src/test/java/server/ServTest.java
@@ -4,7 +4,6 @@ import commons.Admin;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ServTest {
     @Test
