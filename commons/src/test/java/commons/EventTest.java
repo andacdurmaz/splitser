181acf24e6bf9b9dc9ff43d06be76ef4688eae41
Jort Kuipers <j.h.j.kuipers@student.tudelft.nl>
2024-02-22 17:16:46 +0100

    added constructor tests

--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -6,6 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class EventTest {
     @Test
-    void name() {
+    void fullConstructorTest() {
+        Event event = new Event("test", 3, "123456abc");
+        assertEquals("test", event.getTitle());
+        assertEquals(3, event.getAmountOfParticipants());
+        assertEquals("123456abc", event.getEventCode());
     }
+
+    @Test
+    void partialConstructorTest() {
+        Event event = new Event("test", 3);
+        assertEquals("test", event.getTitle());
+        assertEquals(3, event.getAmountOfParticipants());
+    }
+
+    
 }
