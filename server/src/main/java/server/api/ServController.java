2468de58b61e7890a74db67d99cb91a58daef360
tcoman <T.A.Coman@student.tudelft.nl>
2024-02-24 16:42:52 +0100

    Serer controller creation

---
+++ server/src/main/java/server/api/ServController.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2021 Delft University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package server.api;
+
+import commons.Quote;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import server.Serv;
+import server.database.QuoteRepository;
+import server.database.ServRepository;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api/servers")
+public class ServController {
+
+
+    private final ServRepository repo;
+
+    public ServController(ServRepository repo) {
+        this.repo = repo;
+    }
+
+    @GetMapping(path = { "", "/" })
+    public List<Serv> getAll() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getById(@PathVariable("id") long id) {
+        if (id < 0 || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.findById(id).get());
+    }
+
+
+
+    private static boolean isNullOrEmpty(String s) {
+        return s == null || s.isEmpty();
+    }
+
+
+}
