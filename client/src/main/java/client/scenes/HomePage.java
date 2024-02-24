09362f0c47fb1b9376f261855845356ee1719e64
Mete Aksoy <M.Aksoy-1@student.tudelft.nl>
2024-02-24 19:05:26 +0100

    Added the skeleton code for HomePage.java

--- client/src/main/java/client/scenes/HomePage.java
+++ client/src/main/java/client/scenes/HomePage.java
@@ -1,4 +1,55 @@
 package client.scenes;
+import com.google.inject.Inject;
+
+import client.utils.ServerUtils;
+import commons.Quote;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class HomePage {
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+    private ObservableList<Quote> data;
+    @FXML
+    private Label emptyLabel;
+    @FXML
+    private ListView<String> EventsList;
+    @FXML
+    private TableView<Quote> table;
+    @FXML
+    private TableColumn<Quote, String> Event;
+    @FXML
+    private TableColumn<Quote, String> CreatedBy;
+    @FXML
+    private TableColumn<Quote, String> CreationDate;
+
+    @Inject
+    public HomePage(ServerUtils server, MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+    }
+
+    /**
+     * Initializing starting page
+     */
+    public void initialize() {
+        updateLabelVisibility();
+
+        // Add an event listener to see changes in the List "EventsList" to see if its empty or not.
+        // If it is empty make the emptyLabel visible again
+        
+    }
+
+    /**
+     *     If the ListView is empty, make the label visible; otherwise, make it invisible
+      */
+    private void updateLabelVisibility() {
+        emptyLabel.setVisible(EventsList.getItems().isEmpty());
+    }
+
 }
