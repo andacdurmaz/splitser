eacb02019675302e28a6479454d682e9f08784d3
Ivan Nikolov <I.I.Nikolov@student.tudelft.nl>
2024-02-25 15:57:02 +0100

    Added Expense class

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -1,2 +1,102 @@
-package commons;public class Expense {
+package commons;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+    private String name;
+    private double amount;
+
+    private List<User> payingParticipants;
+
+    /*
+        Constructor for the expense class
+     */
+    public Expense (String name, double amount) {
+        this.name = name;
+        this.amount = amount;
+        this.payingParticipants = new ArrayList<>();
+    }
+
+    /*
+        Getter for the name attribute
+     */
+    public String getName() {
+        return name;
+    }
+
+    /*
+        Setter for the name attribute
+     */
+    public void setName(String title) {
+        this.name = name;
+    }
+
+    /*
+        Getter for the amount attribute
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /*
+        Setter for the amount attribute
+     */
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    /*
+        Getter for the paying participants
+     */
+    public List<User> getPayingParticipants() {
+        return payingParticipants;
+    }
+
+    /*
+        Setter for the paying participants
+     */
+    public void setPayingParticipants(List<User> payingParticipants) {
+        this.payingParticipants = payingParticipants;
+    }
+
+    /*
+        Equals method for the Expense class
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Expense expense)) return false;
+        return id == expense.id && Double.compare(getAmount(), expense.getAmount()) == 0 && Objects.equals(getName(), expense.getName()) && Objects.equals(getPayingParticipants(), expense.getPayingParticipants());
+    }
+
+    /*
+        Hash code method for the Expense class
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, getName(), getAmount(), getPayingParticipants());
+    }
+
+    /*
+        ToString method for the expense class
+     */
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", amount=" + amount +
+                ", payingParticipants=" + payingParticipants +
+                '}';
+    }
 }
