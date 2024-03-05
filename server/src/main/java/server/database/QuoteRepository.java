d3373360a7da21e5f4c5cc0e2497b7a86e58c8e0
Ivan Nikolov <I.I.Nikolov-1@student.tudelft.nl>
2024-03-05 16:30:25 +0100

    Remove unused imports

--- server/src/main/java/server/database/QuoteRepository.java
+++ server/src/main/java/server/database/QuoteRepository.java
@@ -18,7 +18,5 @@ package server.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import commons.Quote;
-import org.springframework.stereotype.Repository;
-
 
 public interface QuoteRepository extends JpaRepository<Quote, Long> {}
