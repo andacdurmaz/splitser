fec8085dd289e88121b10e1323f34901fbc186d5
Storm van Wassenaar <s.vanwassenaar@student.tudelft.nl>
2024-03-07 16:43:41 +0100

    Added tests using Mockito

--- server/src/main/java/server/database/AdminRepository.java
+++ server/src/main/java/server/database/AdminRepository.java
@@ -18,7 +18,5 @@ package server.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 import commons.Admin;
 
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-    boolean existsById(long Id);
-    Admin getAdminById(long Id);
+public interface AdminRepository extends JpaRepository<Admin, String> {
 }
