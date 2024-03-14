0398a47c947b6b27f21bfb455b6ffc1535ae9ad0
tcoman <T.A.Coman@student.tudelft.nl>
2024-03-14 17:42:27 +0100

    Added code for the websockets to work

--- client/src/main/java/client/utils/Util.java
+++ client/src/main/java/client/utils/Util.java
@@ -5,22 +5,92 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import org.glassfish.jersey.client.ClientConfig;
 
+import java.net.InetAddress;
+
 public class Util {
+    protected static String address;
+
+    protected static String server;
     @Resource
     protected Client clientBuilder;
 
     /**
-     * Constructor for BaseUtil and all of ServerUtils
+     * Constructor for Util and all of ServerUtils
      */
     public Util() {
         this.clientBuilder = ClientBuilder.newClient(new ClientConfig());
     }
 
     /**
-     * Getter method for the current ClientReturner object
-     * @return a current ClientReturner Object
+     * Getter method for the current ClientBuilder object
+     * @return a current ClientBuilder Object
      */
     public Client getClient() {
         return clientBuilder;
     }
+
+
+    /**
+     * Getter method for the current ClientBuilder object
+     *
+     * @return a current ClientBuilder Object
+     */
+    public Client getClientBuilder() {
+        return clientBuilder;
+    }
+
+    /**
+     * Setter method for the current ClientReturn Object. This is here for testing
+     * purposes to allow overriding the ClientBuilder class. However, normally the
+     * default ClientBuilder is fine and should *not* be changed.
+     * @param clientBuilder The new clientBuilder to change to
+     */
+    public void setClientBuilder(ClientBuilder clientBuilder) {
+        this.clientBuilder = clientBuilder.build();
+    }
+
+    /**
+     * Sets the address of the server
+     * @param address Server address
+     * @return boolean whether connection was successful
+     */
+    public boolean setServer(String address){
+        if(!isValidServerAddress(address)){
+            return false;
+        }
+
+        Util.address = address;
+        Util.server = "http://" + address + "/";
+        return true;
+    }
+
+    /**
+     * Checks if a server address is valid
+     * @param address Address to check
+     * @return Boolean whether the address is valid
+     */
+    private static boolean isValidServerAddress(String address) {
+        try {
+            InetAddress testAddress;
+
+            if(address.equals("localhost:8080")){
+                testAddress = InetAddress.getLocalHost();
+            }else{
+                testAddress = InetAddress.getByName("http://" + address + "/");
+            }
+
+            return testAddress.isReachable(1500);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Gets the address of the server
+     * @return Returns the server address
+     */
+    public static String getServer() {
+        return Util.server;
+    }
+
 }
--- server/src/main/java/server/WebSocketConfig.java
+++ server/src/main/java/server/WebSocketConfig.java
@@ -11,12 +11,25 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    /**
+     * @param registry An endpoint register
+     * This creates a server side registry
+     * with the urls where a server will be
+     * listening for updates
+     */
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/websocket");
     }
 
 
+    /**
+     * @param config the component that responds to a request
+     * This actually enables the websocket,
+     * aka the bilateral connection between
+     * server and client
+     *
+     */
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/event");
