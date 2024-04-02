b801d8ba7a38bb3af31e4cdc28c91f62d521ee65
Storm van Wassenaar <s.vanwassenaar@student.tudelft.nl>
2024-04-02 14:22:07 +0200

    Added translation for the status of download

--- client/src/main/java/client/MyFXML.java
+++ client/src/main/java/client/MyFXML.java
@@ -52,7 +52,7 @@ public class MyFXML {
     public <T> Pair<T, Parent> load(Class<T> c, String... parts) {
         try {
             var loader = new FXMLLoader(getLocation(parts), null, null,
-                    new MyFactory(), StandardCharsets.UTF_8);
+                    new MyFactory(), StandardCharsets.ISO_8859_1);
             Parent parent = loader.load();
             T ctrl = loader.getController();
             return new Pair<>(ctrl, parent);
@@ -72,7 +72,7 @@ public class MyFXML {
     public <T> Pair<T, Parent> load(Class<T> c, ResourceBundle bundle, String... parts) {
         try {
             var loader = new FXMLLoader(getLocation(parts), null, null,
-                    new MyFactory(), StandardCharsets.UTF_8);
+                    new MyFactory(), StandardCharsets.ISO_8859_1);
             loader.setResources(bundle);
             Parent parent = loader.load();
             T ctrl = loader.getController();
