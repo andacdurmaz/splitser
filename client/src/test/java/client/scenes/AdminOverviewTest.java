0d46dd062081d202bc04c0306f636d9fe44134b0
Storm van Wassenaar <s.vanwassenaar@student.tudelft.nl>
2024-04-09 14:20:03 +0200

    Added some tests for adminoverview

--- client/src/test/java/client/scenes/AdminOverviewTest.java
+++ client/src/test/java/client/scenes/AdminOverviewTest.java
@@ -3,9 +3,15 @@ package client.scenes;
 import client.Main;
 import client.utils.ServerUtils;
 import com.google.inject.Stage;
+import commons.Event;
+import commons.Expense;
+import commons.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -22,6 +28,72 @@ public class AdminOverviewTest{
     public void setup() {
         sut = new MainCtrl();
         adminOverviewCtrl = new AdminOverviewCtrl(new ServerUtils(), sut);
+    }
+
+//    @Test
+//    public void createNewExpenseWithExpenses(){
+//        User user1 = new User("User1", "User1@tu.nl");
+//        User user2 = new User("User2", "User2@tu.nl");
+//        ArrayList<User> payingParticipant = new ArrayList<>(List.of(user2));
+//        LocalDate date = LocalDate.of(2024, 3, 3);
+//        Expense expense = new Expense("ExpenseName", 4, user1, payingParticipant,date);
+//        Event event = new Event("Event",2,"Description");
+//        ArrayList<Expense> expenses = new ArrayList<>(List.of(expense));
+//        event.setExpenses(expenses);
+//
+//        for(int i = 0; i<expenses.size(); i++) {
+//            assertEquals(expenses.get(i).getAmount(), adminOverviewCtrl.createNewExpenses(event).get(i).getAmount());
+//            assertNotEquals(expenses.get(i).getId(), adminOverviewCtrl.createNewExpenses(event).get(i).getId());
+//            assertEquals(expenses.get(i).getName(), adminOverviewCtrl.createNewExpenses(event).get(i).getName());
+//        }
+//    }
+
+    @Test
+    public void createNewExpenseWithoutExpenses(){
+        User user1 = new User("User1", "User1@tu.nl");
+        User user2 = new User("User2", "User2@tu.nl");
+        ArrayList<User> payingParticipant = new ArrayList<>(List.of(user2));
+        LocalDate date = LocalDate.of(2024, 3, 3);
+        Event event = new Event("Event",2,"Description");
+        ArrayList<Expense> expenses = new ArrayList<>(List.of());
+        event.setExpenses(expenses);
+
+        assertEquals(expenses, adminOverviewCtrl.createNewExpenses(event));
+    }
+
+//    @Test
+//    public void createNewParticipantsWithParticipants(){
+//        User user1 = new User("User1", "User1@tu.nl");
+//        User user2 = new User("User2", "User2@tu.nl");
+//        ArrayList<User> participants = new ArrayList<>(List.of(user1,user2));
+//        ArrayList<User> payingParticipant = new ArrayList<>(List.of(user2));
+//        LocalDate date = LocalDate.of(2024, 3, 3);
+//        Expense expense = new Expense("ExpenseName", 4, user1, payingParticipant,date);
+//        Event event = new Event("Event",2,"Description");
+//        ArrayList<Expense> expenses = new ArrayList<>(List.of(expense));
+//        event.setExpenses(expenses);
+//        event.setParticipants(participants);
+//
+//        for(int i = 0; i<participants.size(); i++){
+//            assertEquals(participants.get(i).getUsername(), adminOverviewCtrl.createNewParticipants(event).get(i).getUsername());
+//            assertNotEquals(participants.get(i).getUserID(), adminOverviewCtrl.createNewParticipants(event).get(i).getUserID());
+//            assertEquals(participants.get(i).getUsername(), adminOverviewCtrl.createNewParticipants(event).get(i).getUsername());
+//        }
+//    }
+
+    @Test
+    public void createNewParticipantsWithoutParticipants(){
+        User user1 = new User("User1", "User1@tu.nl");
+        User user2 = new User("User2", "User2@tu.nl");
+        ArrayList<User> participants = new ArrayList<>(List.of());
+        ArrayList<User> payingParticipant = new ArrayList<>(List.of(user2));
+        LocalDate date = LocalDate.of(2024, 3, 3);
+        Expense expense = new Expense("ExpenseName", 4, user1, payingParticipant,date);
+        Event event = new Event("Event",2,"Description");
+        ArrayList<Expense> expenses = new ArrayList<>(List.of(expense));
+        event.setExpenses(expenses);
+
+        assertEquals(participants, adminOverviewCtrl.createNewParticipants(event));
     }
 
 }
